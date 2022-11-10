a0f8a628c0706d4bb86a968b21a3c75fb8c04ebc
chonghao.qianchong <chonghao.qianchong@alibaba-inc.com>
2022-11-10 23:08:37 +0800

    [feature] elastic init

---
+++ elasticsearch/src/main/java/com/alibaba/configuration/ElasticSearchClient.java
@@ -0,0 +1,53 @@
+package com.alibaba.configuration;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.io.CharStreams;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+@Component
+public class ElasticSearchClient implements InitializingBean {
+    private RestHighLevelClient highLevelClient;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("elasticsearch.json");
+        String json = CharStreams.toString(new InputStreamReader(is));
+        JSONObject conf = JSONObject.parseObject(json);
+
+        String username = conf.getString("username");
+        String password = conf.getString("password");
+        String host = conf.getString("host");
+        Integer port = conf.getInteger("port");
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        //访问用户名和密码为您创建阿里云Elasticsearch实例时设置的用户名和密码，也是Kibana控制台的登录用户名和密码。
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        // 通过builder创建rest client，配置http client的HttpClientConfigCallback。
+        // 单击所创建的Elasticsearch实例ID，在基本信息页面获取公网地址，即为ES集群地址。
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, 9200, "http"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        // RestHighLevelClient实例通过REST low-level client builder进行构造。
+        RestHighLevelClient highClient = new RestHighLevelClient(builder);
+
+    }
+}
