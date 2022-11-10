f0bc64c374fb59611c92f7a03e03f6dff8c410bf
chonghao.qianchong <chonghao.qianchong@alibaba-inc.com>
2022-11-10 22:31:10 +0800

    init

--- boot/src/main/java/com/alibaba/ChamberApplication.java
+++ boot/src/main/java/com/alibaba/ChamberApplication.java
@@ -1,7 +1,6 @@
 package com.alibaba;
 
 
-import com.alibaba.configuration.ElasticSearchConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
--- elasticsearch/src/main/java/com/alibaba/configuration/ElasticSearchConfig.java
+++ elasticsearch/src/main/java/com/alibaba/configuration/ElasticSearchConfig.java
@@ -1,10 +1,9 @@
 package com.alibaba.configuration;
 
-import com.alibaba.Initializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import org.springframework.stereotype.Component;
+
+@Component
 public class ElasticSearchConfig {
 //    @Bean
 //    public Initializer getInitializer(){
